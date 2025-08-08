106a31dce8098fd72175001d7087d658b58681f0
Breadheadmo <120557853+Breadheadmo@users.noreply.github.com>
2025-08-08 13:11:13 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,19 +2,17 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-})
+  disable: process.env.NODE_ENV === 'development',
+});
 
-module.exports = withPWA({
-  // your existing Next.js config
-})
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/YOUR_REPO_NAME', // e.g. /my-nextjs-site
+  basePath: '/YOUR_REPO_NAME', // Replace with your repo name
   assetPrefix: '/YOUR_REPO_NAME/',
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
