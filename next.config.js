c6ea9a28c793b6a05274437cdf842eff448cb3f4
Breadheadmo <120557853+Breadheadmo@users.noreply.github.com>
2025-08-08 15:11:58 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,16 +7,12 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // for GitHub Pages
-  basePath: '', // or '/REPO_NAME' if deploying to a subpath
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export', // GitHub Pages static export
+  basePath: '', // or '/REPO_NAME'
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   images: {
-    unoptimized: true, // required for static export
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
